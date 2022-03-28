6b130957f08cb96e56f28bcd3ab957df6ccd2731
김성해 <sunghae.kim@mirero.co.kr>
2022-03-28 13:20:30 +0900

    vuedongsan Part 1 ~ 5강 Code Commit

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="menu">
+      <a v-for="item, index in Menu" :key="index">{{Menu[index]}}</a>
+    </div>
+
+    <img alt="Vue logo" src="./assets/logo.png" width="200">
+    <h4>{{LogoTitle}}</h4>
+
+    <div v-for="product, index in Products" :key="index">
+      <h4 :style=myStyle01>{{Products[index].Name}}</h4>
+      <p>{{Products[index].Price}} 만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      LogoTitle : 'One Room Shop',
+      Menu : ['Home', 'Products', 'About'],
+      
+      myStyle01 : 'color : blue',      
+
+      Products : [
+        {
+          Name : '역삼동원룸',
+          Price : 6000,
+        },
+        {
+          Name : '천호동원룸',
+          Price : 7000,
+        },
+        {
+          Name : '마포구원룸',
+          Price : 8000,
+        },
+      ]
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +55,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color : white;
+  padding: 10px;
 }
 </style>
